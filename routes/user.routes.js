21d84d2b8cd73de98ec244cb3363f2a12a5c860f
jmenna <61073675+jmenna@users.noreply.github.com>
2021-02-24 15:42:58 -0500

    added paths

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const userController = require('./controllers/user.controllers');
+const userController = require('../controllers/user.controllers');
 
 router.get('/', userController.getUsers);
 
