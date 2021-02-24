ba7b8926691519301406ba8601c1ce87e889c2e5
jmenna <61073675+jmenna@users.noreply.github.com>
2021-02-24 15:09:49 -0500

    ✨feature: toJSON options

--- models/user.js
+++ models/user.js
@@ -4,7 +4,30 @@ module.exports = mongoose.model('User', new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   age: { type: Number, required: true, min: 0 },
-  ssn: { type: String, required: true, unique: true },
+  ssn: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return /\d{3}-\d{2}-\d{4}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid social security number!`,
+    },
+  },
   address: { type: String, required: false },
-  phoneNumber: { type: String, required: false },
+  phoneNumber: {
+    type: String,
+    required: false,
+    validate: {
+      validator(v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
+  toJSON: {
+    getters: true,
+    virtuals: false,
+  },
 }));
