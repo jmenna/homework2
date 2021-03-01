ced64859fa80e0c47b17bbf2480cc73d8e3dfa57
jmenna <61073675+jmenna@users.noreply.github.com>
2021-03-01 17:45:53 -0500

    added to controller

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,1 +1,14 @@
 const userService = require('../services/services');
+
+exports.getUsers = async function (req, res, next) {
+  // Validate request parameters, queries using express-validator
+
+  const page = req.params.page ? req.params.page : 1;
+  const limit = req.params.limit ? req.params.limit : 10;
+  try {
+    const users = await userService.getUsers({}, page, limit);
+    return res.status(200).json({ status: 200, data: users, message: 'Succesfully Users Retrieved' });
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
