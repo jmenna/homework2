87e0d781e4a08d28e01de5c33a6b73c526933fe5
jmenna <61073675+jmenna@users.noreply.github.com>
2021-03-01 17:41:33 -0500

    made code easier to read

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,7 @@
 const express = require('express');
-
-const router = express.Router();
-
 const userController = require('../controllers/controllers');
 
+const router = express.Router();
 router.get('/', userController.getUsers);
 
 module.exports = router;
